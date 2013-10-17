a354cc40a797b3cb9b0f6930729f26ad4a21557a
Javier Alvarez <api@srv-ruu.mides.local>
2013-10-17 16:57:29 -0600

    Commit Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MidesApi::Application.config.secret_key_base = '5cbc0b08cd45c37e6264c30e69d9ee1d9822bef802f57665936c4063a99866a69a6ac4bb9f8ec63b732b1bcd6aadf97962af8c48e5f03c66e0c9aba77a76ce52'
