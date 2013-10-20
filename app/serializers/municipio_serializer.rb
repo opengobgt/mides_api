c75ce816afee3f3a24757bf0138f7f1ec7b8e38c
Javier Alvarez <jalvarezsamayoa@gmail.com>
2013-10-20 16:04:13 -0600

    Agregado opcion embed=departamento,comunidades,escuelas a Serrializar de Municipios

--- app/serializers/municipio_serializer.rb
+++ app/serializers/municipio_serializer.rb
@@ -1,19 +1,50 @@
 class MunicipioSerializer < ActiveModel::Serializer
-  attributes :id, :nombre, :url, :departamento, :comunidades, :escuelas
+  attributes :id, :nombre, :url, :departamento_url, :comunidades_url, :escuelas_url
+
+  has_one :departamento
+  has_many :comunidades
+  has_many :escuelas
+
 
   def url
     departamento_municipio_url(object.departamento_id, object.id)
   end
 
-  def departamento
-    departamento_url(object.departamento_id)
+  def departamento_url
+    scope.request.protocol + \
+    scope.request.host + \
+    (scope.request.port.to_s rescue '') + \
+    departamento_path(object.departamento_id)
   end
 
-  def comunidades
+  def comunidades_url
     departamento_municipio_comunidades_url(object.departamento_id, object.id)
   end
 
-  def escuelas
+  def escuelas_url
     departamento_municipio_escuelas_url(object.departamento_id, object.id)
   end
+
+  def include_departamento?
+    if scope.params[:embed]
+      embed = scope.params[:embed].split(',') 
+      return true if embed.include?('departamento')
+    end
+  end
+
+
+  def include_comunidades?
+    if scope.params[:embed]
+      embed = scope.params[:embed].split(',') 
+      return true if embed.include?('comunidades')
+    end
+  end
+
+  def include_escuelas?
+    if scope.params[:embed]
+      embed = scope.params[:embed].split(',') 
+      return true if embed.include?('escuelas')
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ MidesApi::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+
   scope '/v1' do
     resources :departamentos, :only => [:index, :show] do
 
