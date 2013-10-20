f6f4cda0757ead91d4d46b3bd57838df924decd2
Javier Alvarez <jalvarezsamayoa@gmail.com>
2013-10-20 16:39:30 -0600

    Agregado embeded=deparatamento,municipio para serializer de comunidad

--- app/serializers/comunidad_serializer.rb
+++ app/serializers/comunidad_serializer.rb
@@ -1,16 +1,37 @@
 class ComunidadSerializer < ActiveModel::Serializer
-  attributes :id, :nombre, :url, :departamento, :municipio
+  attributes :id, :nombre, :url, :departamento_url, :municipio_url
+
+  has_one :departamento
+  has_one :municipio
 
   def url
     departamento_municipio_comunidad_url(object.departamento_id, object.municipio_id, object.id)
   end
 
-  def departamento
-    departamento_url(object.departamento_id)
+  def departamento_url
+    scope.request.protocol + \
+    scope.request.host + \
+    (scope.request.port.to_s rescue '') + \
+    departamento_path(object.departamento_id)
   end
 
-  def municipio
+  def municipio_url
     departamento_municipio_url(object.departamento_id, object.municipio_id)
+  end
+
+  def include_departamento?
+    if scope.params[:embed]
+      embed = scope.params[:embed].split(',') 
+      return true if embed.include?('departamento')
+    end
+  end
+
+
+  def include_municipio?
+    if scope.params[:embed]
+      embed = scope.params[:embed].split(',') 
+      return true if embed.include?('municipio')
+    end
   end
 
 end
