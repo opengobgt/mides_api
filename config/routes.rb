953aa3390826024a5538af22efe4defea5a484b0
Javier Alvarez <jalvarezsamayoa@gmail.com>
2013-10-21 07:38:56 -0600

    configurado departamentos como root

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ MidesApi::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  root 'departamentos#index'
+
 
   scope '/v1' do
     resources :departamentos, :only => [:index, :show] do
